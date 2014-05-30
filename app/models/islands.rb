fba53e4177c47336c094a876b73c99ef4d8fa705
Missy Williams <missywilliams@Missys-MacBook-Pro.local>
2014-05-30 15:19:57 -0500

    Generate islands model

---
+++ app/models/islands.rb
@@ -0,0 +1,2 @@
+class Islands < ActiveRecord::Base
+end
---
+++ db/migrate/20140530201923_create_islands.rb
@@ -0,0 +1,11 @@
+class CreateIslands < ActiveRecord::Migration
+  def change
+    create_table :islands do |t|
+      t.string :Name
+      t.string :Description
+      t.int :Destination_Id
+
+      t.timestamps
+    end
+  end
+end
