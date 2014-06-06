0cab30db084a2e9bfe41fc02361b70aa2149a1db
trey <treygeorge@gmail.com>
2014-06-06 15:32:22 -0500

    fixing link and updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140606184124) do
+ActiveRecord::Schema.define(version: 20140606192742) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -31,7 +30,6 @@ ActiveRecord::Schema.define(version: 20140606184124) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
-
 
   create_table "destinations", force: true do |t|
     t.string   "Name"
