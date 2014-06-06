8fb38abae73689a4a957c86f7a138fd226d0119f
Missy Williams <missywilliams@Missys-MacBook-Pro.local>
2014-06-06 16:09:23 -0500

    Add hotel pictures model because i messed it up earlier, and take out the unecessary migration. then add relations to other models

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,4 @@
 class Hotel < ActiveRecord::Base
+	belongs_to :island
+	has_many :hotel_pictures
 end
--- db/migrate/20140606205654_more_column_names_to_lower_case.rb
+++
@@ -1,6 +0,0 @@
-class MoreColumnNamesToLowerCase < ActiveRecord::Migration
-  def change
-    rename_column :hotel_pictures, :Path, :path
-    rename_column :hotel_pictures, :Hotel_Id, :hotel_id
-  end
-end
