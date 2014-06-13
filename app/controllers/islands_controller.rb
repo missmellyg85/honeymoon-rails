8fd8a13a67661c270fb9b6ab821c1105e58930af
Missy Williams <missywilliams85@gmail.com>
2014-06-13 10:08:42 -0500

    Island controller CRUD. Index view. Working on others

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -2,23 +2,46 @@ class IslandsController < ApplicationController
     before_action :authenticate_admin!
 
     def new
+        @island = Island.new
     end
 
     def create
+        @island = Island.new(island_params)
+
+        if @island.save
+            redirect_to @island, notice: 'Island was created successfully'
+        end
     end
 
     def update
-    end
+        island = Island.find(params[:id])
+
+        if island.update!(island_params)
+            redirect_to island, notice: 'Island was updated successfully'
+    end end
 
     def edit
+        @island = Island.find(params[:id])
     end
 
     def destroy
+        island = Island.find(params[:id])
+
+        if island.destroy
+            redirect_to island_url, notice: 'Island was deleted succesfully'
+        end
     end
 
     def index
+        @islands = Island.all()
     end
 
     def show
+        @island = Island.find(params[:id])
     end
+
+    private
+        def island_params
+            params.require(:island).permit(:name, :path)
+        end
 end
