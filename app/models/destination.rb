f274664312a1981a4f7a85e5a26577dd56e3cef8
trey <treygeorge@gmail.com>
2014-06-21 11:18:04 -0500

    fixes adding empty resources

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,7 @@
 class Destination < ActiveRecord::Base
 	has_many :islands, dependent: :destroy
 
-	accepts_nested_attributes_for :islands, :allow_destroy => true
+	accepts_nested_attributes_for :islands,
+                                :allow_destroy => true,
+                                :reject_if => lambda { |a| a[:name].blank? && a[:description].blank? }
 end
--- app/models/island.rb
+++ app/models/island.rb
@@ -1,6 +1,8 @@
 class Island < ActiveRecord::Base
-	belongs_to :destination
-	has_many :hotels, dependent: :destroy
-	
-	accepts_nested_attributes_for :hotels, :allow_destroy => true
+    belongs_to :destination
+    has_many :hotels, dependent: :destroy
+
+    accepts_nested_attributes_for :hotels,
+                                  :allow_destroy => true,
+                                  :reject_if => lambda { |a| a[:name].blank? && a[:description].blank? }
 end
