13a97fad361abcd12d5d125150efe775bc520f1f
Missy Williams <missywilliams85@gmail.com>
2014-08-22 17:15:56 -0500

    Drop down of all destinations in nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :destinations_list
+  def destinations_list
+	  Destination.all
+	end
 end
